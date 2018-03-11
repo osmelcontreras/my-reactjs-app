687ae19ee007c29d2203c9c6f07b486d63474b88
Osmel Contreras <osmeljohan@gmail.com>
2018-03-11 03:03:50 -0400

    added a new line

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
   }
   
   handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slic(0, this.state.stepNumber + 1);
 		const current = history[history.length -1];
 		const squares = current.squares.slice();
 		if(calculateWinner(squares) || squares[i]) {
@@ -71,6 +71,7 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
 		});
 	}
